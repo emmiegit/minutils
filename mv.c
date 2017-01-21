b9e0d368860be3f9baf0a3dc8e7700721f73bcb7
Ammon Smith <ammon.i.smith@gmail.com>
2017-01-20 20:49:24 -0800

    Add copy file for cross-device move.

--- mv.c
+++ mv.c
@@ -3,18 +3,50 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <libgen.h>
 #include <unistd.h>
 
 #include <errno.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-/*
- * NOTE:
- * This program can only move files that are on the same filesystem.
- */
+static int copy_file(const char *source, int destfd, const char *destfn)
+{
+	char buf[4096];
+	ssize_t len;
+	int ifd, ofd;
 
-/* Usage: ./mv original... destination */
+	ifd = open(source, O_RDONLY);
+	if (ifd < 0) {
+		return 1;
+	}
+	ofd = openat(destfd, destfn, O_CREAT | O_TRUNC | O_WRONLY, 0666);
+	if (ofd < 0) {
+		return 1;
+	}
+
+	do {
+		len = read(ifd, buf, sizeof(buf));
+		if (len < 0) {
+			return 1;
+		} else if (len == 0) {
+			return 0;
+		}
+		if (write(ofd, buf, len) != len) {
+			return 1;
+		}
+	} while (len == sizeof(buf));
+	if (close(ifd) || close(ofd)) {
+		return 1;
+	}
+	if (unlink(source)) {
+		return 1;
+	}
+	return 0;
+}
+
+/* Usage: ./mv files... dest */
 int main(int argc, char *argv[])
 {
 	if (argc == 1) {
@@ -44,11 +76,25 @@ int main(int argc, char *argv[])
 			return 1;
 		}
 		for (i = 1; i < argc - 1; i++) {
-			if (renameat(AT_FDCWD, argv[i], destfd, argv[i])) {
+			char *copy, *destfn;
+
+			copy = strdup(argv[i]);
+			if (!copy) {
+				fprintf(stderr, "%s: %s\n",
+					argv[0], strerror(errno));
+				return 1;
+			}
+			destfn = basename(copy);
+			if (renameat(AT_FDCWD, argv[i], destfd, destfn)) {
+				if (errno == EXDEV && !copy_file(argv[i], destfd, destfn)) {
+					free(copy);
+					continue;
+				}
 				fprintf(stderr, "%s: %s: %s\n",
 					argv[0], argv[i], strerror(errno));
 				return 1;
 			}
+			free(copy);
 		}
 		if (close(destfd)) {
 			fprintf(stderr, "%s: %s: %s\n",
