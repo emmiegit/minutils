e6f2610b3ce8df0ccd45f39c3f7cdd6db4eedce3
Ammon Smith <ammon.i.smith@gmail.com>
2017-01-21 13:30:03 -0800

    Add sleep.

---
+++ sleep.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	char *ptr;
+	long val;
+
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s seconds\n", argv[0]);
+		return 1;
+	}
+	val = strtol(argv[1], &ptr, 10);
+	if (*ptr || val < 0) {
+		fprintf(stderr, "%s: not a positive integer: %s\n",
+			argv[0], argv[1]);
+		exit(1);
+	}
+	if (sleep(val)) {
+		return 1;
+	}
+	return 0;
+}
+
