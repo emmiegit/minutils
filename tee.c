c364fd9f514f8d256323175f98202445f5d86f0c
Ammon Smith <ammon.i.smith@gmail.com>
2017-01-20 17:45:21 -0800

    Fix tee.c

--- tee.c
+++ tee.c
@@ -1,34 +1,49 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Usage: ./tee [file...] */
 int main(int argc, char *argv[])
 {
-	FILE **fh;
-	int ch, i;
+	char buf[4096];
+	ssize_t n;
+	int *fds;
+	int i, ret;
 
-	fh = malloc(sizeof(FILE *) * (argc - 1));
-	if (!fh) {
+	fds = malloc(sizeof(int) * argc);
+	if (!fds) {
+		fprintf(stderr, "%s: unable to allocate: %s\n",
+			argv[0], strerror(errno));
 		return -1;
 	}
-
-	for (i = 0; i < argc - 1; i++) {
-		if ((fh[i] = fopen(argv[i + 1], "w+")) == NULL) {
+	fds[0] = STDOUT_FILENO;
+	for (i = 1; i < argc; i++) {
+		fds[i] = open(argv[i], O_CREAT | O_TRUNC | O_WRONLY, 0666);
+		if (!fds[i]) {
 			return 1;
 		}
 	}
-
-	while ((ch = fgetc(stdin)) != EOF) {
-		fputc(ch, stdout);
-		for (i = 0; i < argc - 1; i++) {
-			fputc(ch, fh[i]);
+	while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+		for (i = 0; i < argc; i++) {
+			if (write(fds[i], buf, n) != n) {
+				fprintf(stderr, "%s: %s: %s\n",
+					argv[0], argv[i], strerror(errno));
+				return 1;
+			}
 		}
 	}
 
-	for (i = 0; i < argc - 1; i++) {
-		fclose(fh[i]);
+	ret = 0;
+	for (i = 1; i < argc; i++) {
+		if (close(fds[i])) {
+			ret = 1;
+		}
 	}
-
-	return 0;
+	free(fds);
+	return ret;
 }
 
