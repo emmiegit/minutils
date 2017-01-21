45c2b737a1bd1fc485b23a52f6a3224dd7acdbbe
Ammon Smith <ammon.i.smith@gmail.com>
2017-01-20 16:39:31 -0800

    Add -a to ls.

--- ls.c
+++ ls.c
@@ -1,37 +1,56 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <string.h>
 
-/* Usage: ls [path] */
+static struct {
+	int all;
+} opt;
+
+static int list(const char *path)
+{
+	DIR *dh;
+	struct dirent *ent;
+
+	dh = opendir(path);
+	if (!dh) {
+		return 1;
+	}
+	while ((ent = readdir(dh))) {
+		if (ent->d_name[0] == '.' && !opt.all) {
+			continue;
+		}
+		printf("%s\n", ent->d_name);
+	}
+	if (closedir(dh)) {
+		return 1;
+	}
+	return 0;
+}
+
+/* Usage: ls [-a] [path...] */
 int main(int argc, const char *argv[])
 {
-	DIR *dirh;
-	struct dirent *dirent;
-	const char *path;
-	int ret;
+	int i;
 
-	if (argc == 1) {
-		path = ".";
-	} else if (argc == 2) {
-		path = argv[1];
-	} else {
-		return 1;
+	for (i = 1; i < argc; i++) {
+		if (argv[i][0] != '-') {
+			break;
+		} else if (!strcmp(argv[i], "-a")) {
+			opt.all = 1;
+		} else {
+			return 1;
+		}
 	}
 
-	dirh = opendir(path);
-	if (!dirh) {
-		return 1;
+	if (i == argc) {
+		list(".");
+	} else if (i == argc - 1) {
+		list(argv[i]);
+	} else for (; i < argc; i++) {
+		printf("%s:\n\n", argv[i]);
+		list(argv[i]);
 	}
-
-	while ((dirent = readdir(dirh))) {
-		printf("%s\n", dirent->d_name);
-	}
-
-	ret = closedir(dirh);
-	if (ret) {
-		return 1;
-	}
-
 	return 0;
 }
 
