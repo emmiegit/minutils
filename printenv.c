24da10a7035e53398e87ae356ba03174fe03ce14
Ammon Smith <ammon.i.smith@gmail.com>
2017-01-24 21:21:04 -0800

    Add printenv.

---
+++ printenv.c
@@ -0,0 +1,102 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static struct {
+	unsigned null : 1;
+} opt;
+
+static int putnull(const char *str)
+{
+	size_t len;
+
+	len = strlen(str);
+	if (fwrite(str, len + 1, 1, stdout) != 1) {
+		return EOF;
+	}
+	return 0;
+}
+
+static void print_all(const char *argv0, const char *environ[])
+{
+	int (*print_func)(const char *);
+
+	print_func = opt.null ? putnull : puts;
+	while (*environ) {
+		if (print_func(*environ++) == EOF) {
+			fprintf(stderr, "%s: error while writing\n",
+				argv0);
+			exit(1);
+		}
+	}
+}
+
+static char *format_var(const char *key, const char *val)
+{
+	size_t keylen, vallen;
+	char *buf;
+
+	keylen = strlen(key);
+	vallen = strlen(val);
+	buf = malloc(keylen + vallen + 2);
+	sprintf(buf, "%s=%s", key, val);
+	return buf;
+}
+
+static void print_vars(const char *argv0, int len, const char *names[])
+{
+	int (*print_func)(const char *);
+	int i, ret;
+
+	ret = 0;
+	print_func = opt.null ? putnull : puts;
+	for (i = 0; i < len; i++) {
+		const char *value;
+		char *buf;
+
+		value = getenv(names[i]);
+		if (!value) {
+			ret = 1;
+			continue;
+		}
+		buf = format_var(names[i], value);
+		if (!buf) {
+			fprintf(stderr, "%s: unable to allocate: %s\n",
+				argv0, strerror(errno));
+			exit(1);
+		}
+		if (print_func(buf) == EOF) {
+			fprintf(stderr, "%s: error while writing\n",
+				argv0);
+			exit(1);
+		}
+		free(buf);
+	}
+	exit(ret);
+}
+
+/* Usage: ./printenv [-0] [variable...] */
+int main(int argc, const char *argv[], const char *environ[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++) {
+		if (argv[i][0] != '-') {
+			break;
+		} else if (!strcmp(argv[i], "-0")) {
+			opt.null = 1;
+		} else {
+			fprintf(stderr, "%s: invalid argument: %s\n",
+				argv[0], argv[i]);
+			return 1;
+		}
+	}
+
+	if (i == argc) {
+		print_all(argv[0], environ);
+	} else {
+		print_vars(argv[0], argc - i, argv + i);
+	}
+	return 0;
+}
