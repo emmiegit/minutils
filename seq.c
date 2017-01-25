8b8ff588ba570d501b0876e7918125cb8b38ae4a
Ammon Smith <ammon.i.smith@gmail.com>
2017-01-25 12:58:20 -0800

    Finish seq.

--- seq.c
+++ seq.c
@@ -12,19 +12,26 @@ static struct {
 	double increment;
 } opt;
 
+#define ISNAN(x)	((x) != (x))
+
 static double parse_float(const char *str)
 {
 	char *ptr;
 	double val;
 
-	val = strtod(str, &ptr, 0);
+	val = strtod(str, &ptr);
 	if (*ptr) {
-		fprintf(stderr, "%s:
+		fprintf(stderr, "%s: invalid float-point argument: %s\n",
+			opt.argv0, str);
+		exit(1);
+	}
+	return val;
 }
 
 /* Usage: ./seq [-s=SEPARATOR] [FIRST] [INCREMENT] LAST */
 int main(int argc, const char *argv[])
 {
+	double n;
 	int i;
 
 	opt.argv0 = argv[0];
@@ -35,13 +42,63 @@ int main(int argc, const char *argv[])
 		} else if (!strncmp(argv[i], "-s=", 3)) {
 			opt.sep = argv[i] + 3;
 		} else {
+			char *ptr;
+
+			strtod(argv[i], &ptr);
+			if (!*ptr) {
+				break;
+			}
 			fprintf(stderr, "%s: invalid argument: %s\n",
-				argv[0], strerror(errno));
+				argv[0], argv[i]);
 			return 1;
 		}
 	}
 	switch (argc - i) {
 	case 1:
-		;
+		opt.first = 1;
+		opt.last = parse_float(argv[i]);
+		opt.increment = 1;
+		break;
+	case 2:
+		opt.first = parse_float(argv[i]);
+		opt.last = parse_float(argv[i + 1]);
+		opt.increment = 1;
+		break;
+	case 3:
+		opt.first = parse_float(argv[i]);
+		opt.last = parse_float(argv[i + 2]);
+		opt.increment = parse_float(argv[i + 1]);
+		break;
+	case 0:
+		fprintf(stderr, "%s: missing operand\n",
+			opt.argv0);
+		return 1;
+	default:
+		fprintf(stderr, "%s: extra operand: %s\n",
+			opt.argv0, argv[i + 3]);
+		return 1;
 	}
+
+	if (opt.increment == 0) {
+		fprintf(stderr, "%s: zero increment\n", opt.argv0);
+		return 1;
+	}
+	if (ISNAN(opt.first) || ISNAN(opt.last) || ISNAN(opt.increment)) {
+		fprintf(stderr, "%s: nan values are disallowed\n", opt.argv0);
+		return 1;
+	}
+	if ((opt.increment > 0) != (opt.first < opt.last)) {
+		/* Can't reach target */
+		return 0;
+	}
+	if (opt.increment > 0) {
+		for (n = opt.first; n <= opt.last; n += opt.increment) {
+			printf("%g\n", n);
+		}
+	} else {
+		for (n = opt.first; n >= opt.last; n += opt.increment) {
+			printf("%g\n", n);
+		}
+	}
+	return 0;
 }
