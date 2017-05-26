f323113d89247fbf36ac6264c292886aa97d93cb
Ammon Smith <ammon.i.smith@gmail.com>
2017-05-26 00:53:38 -0700

    Have basename and dirname comply with standards.

--- src/basename.c
+++ src/basename.c
@@ -1,14 +1,42 @@
 #include <libgen.h>
 
 #include <stdio.h>
+#include <string.h>
 
-/* Usage: basename PATH */
+static void remove_suffix(char *base, const char *suffix)
+{
+	size_t baselen, suflen;
+	char *ptr;
+
+	baselen = strlen(base);
+	suflen = strlen(suffix);
+
+	ptr = base + baselen - suflen;
+	if (!strcmp(ptr, suffix))
+		*ptr = '\0';
+}
+
+/* Usage: basename PATH [SUFFIX] */
 int main(int argc, char *argv[])
 {
-	if (argc == 1) {
+	const char *suffix;
+	char *base;
+
+	switch (argc) {
+	case 2:
+		suffix = NULL;
+		break;
+	case 3:
+		suffix = argv[2];
+		break;
+	default:
 		fprintf(stderr, "%s: missing operand\n", argv[0]);
 		return 1;
 	}
-	puts(basename(argv[1]));
+	base = basename(argv[1]);
+	if (suffix)
+		remove_suffix(base, suffix);
+
+	puts(base);
 	return 0;
 }
--- src/dirname.c
+++ src/dirname.c
@@ -2,13 +2,17 @@
 
 #include <stdio.h>
 
-/* Usage: dirname PATH */
+/* Usage: dirname PATH... */
 int main(int argc, char *argv[])
 {
-	if (argc == 1) {
+	int i;
+
+	if (argc < 2) {
 		fprintf(stderr, "%s: missing operand\n", argv[0]);
 		return 1;
 	}
-	puts(dirname(argv[1]));
+
+	for (i = 1; i < argc; i++)
+		puts(dirname(argv[i]));
 	return 0;
 }
